c26be441a7f7098d0aa6012702cefe87934ea508
Spencer <spencer@spencersmith.site>
2025-06-30 16:17:00 -0400

    additional plugins

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -10,23 +10,23 @@ vim.opt.incsearch = true
 
 -- Diagnostics options
 vim.diagnostic.config({
-    --    virtual_lines = {
-    --    current_line = true
-    --    },
-    virtual_text = true,
-    underline = true
-    --},
+	--    virtual_lines = {
+	--    current_line = true
+	--    },
+	virtual_text = true,
+	underline = true,
+	--},
 })
 
 -- Cursor behavior
 vim.opt.startofline = true
 
--- Turn off line wrap
-vim.opt.wrap = false
+-- Turn on line wrap
+vim.opt.wrap = true
 vim.opt.colorcolumn = "79"
 
 -- Keep extra lines when scrolling
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 10
 
 -- Indentation
 vim.opt.expandtab = true
@@ -37,34 +37,34 @@ vim.opt.autoindent = true
 vim.opt.smartindent = true
 
 -- Format on save
-vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*",
-    callback = function()
-        vim.lsp.buf.format({ async = true })
-    end,
-})
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--     pattern = "*",
+--     callback = function()
+--         vim.lsp.buf.format({ async = true })
+--     end,
+-- })
 
 -- File handling
 vim.cmd("filetype plugin indent on")
 
 -- Transparency settings
 local function set_transparent_bg()
-    local highlights = {
-        "Normal",
-        "NormalFloat",
-        "NormalNC",
-        "SignColumn",
-        "VertSplit",
-        "StatusLine",
-        "LineNr",
-        "CursorLineNr",
-        "EndOfBuffer",
-        "NonText",
-    }
+	local highlights = {
+		"Normal",
+		"NormalFloat",
+		"NormalNC",
+		"SignColumn",
+		"VertSplit",
+		"StatusLine",
+		"LineNr",
+		"CursorLineNr",
+		"EndOfBuffer",
+		"NonText",
+	}
 
-    for _, hl in ipairs(highlights) do
-        vim.api.nvim_set_hl(0, hl, { bg = "none" })
-    end
+	for _, hl in ipairs(highlights) do
+		vim.api.nvim_set_hl(0, hl, { bg = "none" })
+	end
 end
 
 -- Apply transparency
@@ -72,5 +72,5 @@ set_transparent_bg()
 
 -- Reapply transparency after colorscheme changes
 vim.api.nvim_create_autocmd("ColorScheme", {
-    callback = set_transparent_bg,
+	callback = set_transparent_bg,
 })
